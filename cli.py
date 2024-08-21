4cef97eaaf4b9f3909f975f61469063e64454748
PostoenkoVI <v.i.postoenko@gmail.com>
2024-08-21 17:41:55 +0300

    intermediate saving

--- cli.py
+++ cli.py
@@ -18,14 +18,50 @@ def run():
     parser.add_argument('-log_path', nargs='?', help='path to logging file', type=str, default='./logs.txt', const='./logs.txt')
     # parser.add_argument('-example_cfg', nargs='?', help='Path to create example config .ini file or not if not stated', type=str, default='', const='')
     parser.add_argument('-full_uniprot_fasta', nargs='?', help='Path to full bacterial SwissProt+TrEMBL .fasta file to search organisms in', type=str, default='', )
-    parser.add_argument('-uniprot_fold', nargs='*', help='Path to folder to store .fasta files splited by taxonomic identifiers from uniprot', type=str, default='', )
-    parser.add_argument('-swissprot_fold', nargs='*', help='Path to folder to store .fasta files splited by taxonomic identifiers from swissprot', type=str, default='', )
+    parser.add_argument('-uniprot_folder', nargs='?', help='Path to folder to store .fasta files splited by taxonomic identifiers from uniprot', type=str, default='', )
+    parser.add_argument('-swissprot_folder', nargs='?', help='Path to folder to store .fasta files splited by taxonomic identifiers from swissprot', type=str, default='', )
     
-    parser.add_argument('-outdir', nargs='?', help='name of directory to store results', type=str, default='', const='')
-    # parser.add_argument('-feature_folder', nargs='?', help='directory to store features', type=str, default='', const='')
+    parser.add_argument('-input_files', nargs='?', help='input .raw or .mzML files to search, separated by whitespace', type=str, default='',)
+    parser.add_argument('-raw_folder', nargs='?', help='Input directory with .raw files', type=str, default='', const='')
+    parser.add_argument('-outdir', nargs='?', help='Output directory', type=str, default='', const='')
+    parser.add_argument('-mzML_folder', nargs='?', help='Directory to search or to store .mzML files after convertation', type=str, default='', const='')
+    parser.add_argument('-feature_folder', nargs='?', help='Path to folder where to store and search for biosaur2 features .tsv files', type=str, default='', const='')
 
     parser.add_argument('-decoy_prefix', nargs='?', help='String added to the protein name to showcase that it is a decoy (default: DECOY)', type=str, default='DECOY', const='DECOY')
+    parser.add_argument('-sprot_suf', nargs='?', help='String added to the taxid of the organism\'s .fasta file to showcase swissprot database (default: _sp)', type=str, default='_sp', const='_sp')
+    parser.add_argument('-uniprot_suf', nargs='?', help='String added to the taxid of the organism\'s .fasta file to showcase uniprot database (default: _sp)', type=str, default='_un', const='_un') 
+    parser.add_argument('-min_pept_length', nargs='?', help='Minimal peptide lenght to use in cleaving 10% database, lowering the value is not recommended (default: 9)', type=int, default=9, const=9)
+    parser.add_argument('-missed_cleavages', nargs='?', help='Number of missed cleavages for cleaving peptide in both searches (default: 0)', type=int, default=0, const=0)
+    parser.add_argument('-cleavage_rule', help='cleavage rule in quotes!. X!Tandem style for cleavage rules: "[RK]|{P}" for trypsin, "[X]|[D]" for asp-n or "[RK]|{P},[K]|[X]" for mix of trypsin and lys-c', default='[RK]|{P}')
+    parser.add_argument('-cmin', help='Min precursor charge', default=2, type=int, const=3)
+    parser.add_argument('-cmax', help='Max precursor charge', default=3, type=int, const=3)
+    parser.add_argument('-mass_accuracy', help='Mass accuracy in ppm for initial search', default=4, type=float, const=4)
+    parser.add_argument('-mz_for_mass_accuracy', help='Approximate maximum m/z value to use with mass_accuracy', default=1000, type=float, const=1000)
+    parser.add_argument('-allowed_ranks', nargs='?', help='Allowed taxonomy categories between <...> and <...>, default="strain,subspecies,forma specialis,isolate,serotype,serogroup,no rank" ', type=str, default='strain,subspecies,forma specialis,isolate,serotype,serogroup,no rank',)
+    ##############
+    parser.add_argument('-score_threshold', help='', default=4, type=float, const=4)
+    parser.add_argument('-rewrite_individual_taxid_fasta', help='', default=0, type=int, const=0, choice=[0, 1])
+    parser.add_argument('-rewrite_10per_fasta', help='', default=0, type=int, const=0, choice=[0, 1])
+    parser.add_argument('-rewrite_blind_search', help='', default=0, type=int, const=0, choice=[0, 1])
+    # self.aa_mass = args['aa_mass'] # см. парсинг в ms1searchpy
+    
+    # может в единый dict их засунуть?
+    # self.path_to_uniprot_taxid_set = args['path_to_uniprot_taxid_set']
+    # self.path_to_sprot_taxid_set = args['path_to_sprot_taxid_set']
+    # self.path_to_len_fasta_uniprot = args['path_to_len_fasta_uniprot']
+    # self.path_to_len_fasta_sprot = args['path_to_len_fasta_sprot']
+    # self.path_to_species_descendants = args['path_to_species_descendants']
+    # self.path_to_leaders_uniprot = args['path_to_leaders_uniprot']
+    # self.path_to_leaders_sprot = args['path_to_leaders_sprot']
+    # self.path_to_10_perc_fasta = args['path_to_10_perc_fasta']
+    # self.path_to_output_prot_set = args['path_to_output_prot_set']
+    # self.path_to_output_specmap_id = args['path_to_output_specmap_id']
+    # self.path_to_output_cnt_to_spec = args['path_to_output_cnt_to_spec']
+    # self.path_to_out_strain_statistics = args['path_to_out_strain_statistics']
+    # self.path_to_top15_leaders_fasta = args['path_to_top15_leaders_fasta']
+    ##############
 
+    
     loglevel = args['logs'].upper()
     numeric_level = getattr(logging, loglevel, None)
     if not isinstance(numeric_level, int):
--- workflow.py
+++ workflow.py
@@ -1,178 +1,119 @@
-from os import path
+from os import path, listdir
 import pickle
 import logging
 from .utils import parse_fasta_for_organisms, calc_lens_for_taxid_set, calc_lens_for_taxid_set, get_descendents_dict, get_leaders, exclude_wrong
 
-def forming_dicts(
-    path_to_uniprot:str='/home/fasta/uniprot_bacteria.fasta',
-    path_to_uniprot_dbs:str='/home/kae-13-1/fasta/bacts_bases_uniprot/',
-    path_to_swissprot_dbs:str='/home/kae-13-1/fasta/bacts_bases_sprot/',
-    
-    path_to_len_fasta_uniprot:str='', 
-    path_to_len_fasta_sprot:str='', 
-    path_to_species_descendants:str='',
-    path_to_leaders_uniprot:str='',
-    path_to_leaders_sprot:str='',
-    path_to_exclude_names:str='',
-    
-    allowed_ranks=('strain', 'subspecies', 'forma specialis', 'isolate', 'serotype', 'serogroup', 'no rank'),
-    rewrite:bool=False,
-) :
-    
-    if not path.exists(path_to_uniprot_dbs) :
-        os.makedirs(path_to_uniprot_dbs, exist_ok=True)
-    
-    if not path.exists(path_to_swissprot_dbs) :
-        os.makedirs(path_to_swissprot_dbs, exist_ok=True)
-    
-    uniprot_taxid_set, swissprot_taxid_set = parse_fasta_for_organisms(path_to_uniprot, 
-                                                                       path_to_uniprot_dbs, 
-                                                                       path_to_swissprot_dbs)
-    
-    if (not path.exists(path_to_len_fasta_uniprot)) or (rewrite) :
-        len_fasta_uniprot = calc_lens_for_taxid_set(uniprot_taxid_set, 
-                                                    path_to_uniprot_dbs, 
-                                                    path_to_dump=path_to_len_fasta_uniprot)
-    else :
-        with open(path_to_len_fasta_uniprot, 'rb') as f :
-            len_fasta_uniprot = pickle.load(f) 
-    
-    if (not path.exists(path_to_len_fasta_sprot)) or (rewrite) :
-        len_fasta_sprot = calc_lens_for_taxid_set(swissprot_taxid_set, 
-                                                  path_to_swissprot_dbs, 
-                                                  path_to_dump=path_to_len_fasta_sprot, 
-                                                  swissprot=True)
-    else :
-        with open(path_to_len_fasta_sprot, 'rb') as f :
-            len_fasta_sprot = pickle.load(f) 
-    
-    if (not path.exists(path_to_species_descendants)) or (rewrite) :
-        species_descendants = get_descendents_dict(uniprot_taxid_set, 
-                                                   allowed_ranks, 
-                                                   path_to_species_descendants=path_to_species_descendants) 
-    else :
-        with open(path_to_species_descendants, 'rb') as f :
-            species_descendants = pickle.load(f)
 
-    if (not path.exists(path_to_leaders_uniprot)) or (not path.exists(path_to_leaders_sprot)) or rewrite :
-        species_leader_uniprot, species_leader_sprot = get_leaders(species_descendants, 
-                                                                   swissprot_taxid_set, 
-                                                                   len_fasta_uniprot, 
-                                                                   len_fasta_sprot, 
-                                                                   path_to_leaders_uniprot=path_to_leaders_uniprot, 
-                                                                   path_to_leaders_sprot=path_to_leaders_sprot)
-    else :
-        with open(path_to_leaders_uniprot, 'rb') as f :
-            species_leader_uniprot = pickle.load(f)
-        with open(path_to_leaders_sprot, 'rb') as f :
-            species_leader_sprot = pickle.load(f)
-    
-    if (not path.exists(path_to_exclude_names)) or (rewrite) :
-        leaders = set(species_leader_sprot.values()).union(set(species_leader_uniprot.values()))
-        exclude_names = exclude_wrong(leaders, path_to_exclude_names=path_to_exclude_names)
-    else :
-        with open(path_to_exclude_names, 'rb') as f :
-            exclude_names = pickle.load(f)
+def feature_generation(mzml_paths, feature_folder, path_to_fd='', str_of_other_args='') :
+    for mzml_path in mzml_paths :
+        outpath = path.join(feature_folder, path.basename(mzml_path).replace('.mzML', '_features.tsv') )
+        call_Biosaur2(path_to_fd, mzml_path, outpath, str_of_other_args=str_of_other_args) :
             
-    return species_leader_uniprot, species_leader_sprot, len_fasta_uniprot, len_fasta_sprot, exclude_names
-
-###
-# NOT WORKING YET
-###
-def blind_search(species_leader_uniprot, 
-                 species_leader_sprot, 
-                 len_fasta_uniprot, 
-                 len_fasta_sprot, 
-                 exclude_names,
-                 
-                 outfasta_path:str='',
-                ) :
-    
-    feature_tsv_path = '/home/lab006/data/VGNKI/2024-04-12_microLC/features/B2P1_ms1_3ug.features.tsv'
-    df1 = prepare_df(feature_tsv_path, charge_range=(2, 3), nIsotopes=3, mz_step=mz_step)
-    df1.head()
-    
-    cnt, top_5_k, md_ar1, id_ar1 = get_matches(df1, [-mass_accuracy, mass_accuracy], accurate_mz_map, protsN, score_threshold=4.0)
-    
-    md_ar2 = []
-    for z1, z2 in zip(md_ar1, id_ar1):
-        if cnt_to_spec[z2-1] in top_5_k:
-            md_ar2.append(z1)
-
-    shift, sigma = optimize_md(md_ar2)
-    print('Optimized mass shift and sigma: ', shift, sigma)
-
-    custom_range_mass_accuracy = [shift-2*sigma, shift+2*sigma]
-    
-    cnt, _, md_ar1, id_ar1 = get_matches(df1, custom_range_mass_accuracy, accurate_mz_map, protsN, score_threshold=4.0)
-    
-    spec_map_id_reversed = dict()
-    for k, v in spec_map_id.items():
-        spec_map_id_reversed[v] = k
-    
-    top_species_names = get_top_species_names(cnt, 
-                                              spec_map_id_reversed, 
-                                              len_fasta_uniprot, 
-                                              exclude_names, 
-                                              number_of_top_proteins=15, 
-                                              max_len_uniprot=220000
-                                             )
-    
-    exitscore = write_top_species_fasta(top_species_names, 
-                                        path_to_fasta_dir_sprot='/home/lerost/Metaproteomics-DirectMS1/Metaproteomics-DirectMS1_close_rep/testing_folder/test1/sprot_fasta_fold/', 
-                                        path_to_fasta_dir_uniprot='/home/lerost/Metaproteomics-DirectMS1/Metaproteomics-DirectMS1_close_rep/testing_folder/test1/uniprot_fasta_fold/', 
-                                        outfasta_path='/home/lerost/Metaproteomics-DirectMS1/Metaproteomics-DirectMS1_close_rep/testing_folder/test1/top_species.fasta', 
-                                        out_strain_statistics_path='/home/lerost/Metaproteomics-DirectMS1/Metaproteomics-DirectMS1_close_rep/testing_folder/test1/B2P1_ms1_3ug.strain_statistics.csv',
-                                       ) 
-    print(exitscore)
-
-def process_files(args) :
-    ncbi = NCBITaxa()
-    species_leader_uniprot, species_leader_sprot, len_fasta_uniprot, len_fasta_sprot, exclude_names = forming_dicts(
-            path_to_uniprot='/home/fasta/uniprot_bacteria.fasta',
-            path_to_uniprot_dbs='/home/kae-13-1/fasta/bacts_bases_uniprot/',
-            path_to_swissprot_dbs='/home/kae-13-1/fasta/bacts_bases_sprot/',
-
-            path_to_len_fasta_uniprot='',
-            path_to_len_fasta_sprot='',
-            path_to_species_descendants='',
-            path_to_leaders_uniprot='',
-            path_to_leaders_sprot='',
-            path_to_exclude_names='',
+            
+def mzml_generation(raw_paths, mzml_folder, path_to_parser='', str_of_other_args='') :
+    for raw_path in raw_paths :
+        outpath = path.join(feature_folder, path.basename(raw_path).replace('.raw', '.mzML') )
+        call_ThermoRawFileParser(path_to_parser, raw_path, outpath, str_of_other_args=str_of_other_args)
         
-            allowed_ranks=('strain', 'subspecies', 'forma specialis', 'isolate', 'serotype', 'serogroup', 'no rank'),
-            rewrite=False,
-        )
+        
+def read_paths(args) :
+    all_paths = dict()
+    rewrite = dict()
     
-    if 
-        exitscore = write_10_perc_fasta(outfasta_path='', 
-                                        path_to_fasta_dir_uniprot='', 
-                                        len_uniprot=len_fasta_uniprot, 
-                                        len_sprot=len_fasta_sprot, 
-                                        species_leader_uniprot=species_leader_uniprot,
-                                        species_leader_sprot=species_leader_sprot, 
-                                        treshold=200
-                                       )
-        logger.debug('write_10_perc_fasta : {}', exitscore)
+    # input
+    
+    input_type=False
+    samples = []
+    if args['input_files'] :
+        input_type = args['input_files'].split(' ')[0].split('.')[-1].lower()
+        all_paths[input_type] = {}
+        for input_file in args['input_files'].split(' ') :
+            sample = path.basename(input_file.strip()).split('.')[0]
+            samples.append(sample)            
+            all_paths[input_type][sample] = input_file.strip()
+        
+    elif args['raw_folder'] :
+        input_type = 'raw'
+        all_paths[input_type] = {}
+        folder = path.abspath(args['raw_folder'])
+        for input_file in os.listdir(folder) :
+            if input_file.lower().endswith('.raw') :
+                sample = input_file.split('.')[0]
+                samples.append(sample)
+                all_paths[input_type][sample] = input_file
+            
+    elif args['mzml_folder'] :
+        input_type = 'mzml'
+        all_paths[input_type] = {}
+        folder = path.abspath(args['mzml_folder'])
+        for input_file in os.listdir(folder) :
+            if input_file.lower().endswith('.mzml') :
+                sample = input_file.split('.')[0]
+                samples.append(sample)
+                all_paths[input_type][sample] = input_file
     else :
-        pass
+        logger.critical('No input files added. At least one of the arguments "input_files", "raw_folder", "mzml_folder" should be filled.')
+        return -1, -1
+        
+    if input_type = 'mzml' :
+        for sample in samples :
+            all_paths['raw'][sample] = ''
+        
     
-    aa_mass = prepare_aa_mass()
-    
-    prot_sets,spec_map_id,cnt_to_spec = prepare_protein_set(
-                                                            fasta_path='', 
-                                                            decoy_prefix='DECOY_', 
-                                                            cleave_rule='[RK]', 
-                                                            min_length=9, 
-                                                            missed_cleavages=0, 
-                                                            aa_mass=aa_mass,
-                                                            path_to_output_prot_set=path_to_output_prot_set, 
-                                                            path_to_output_specmap_id=path_to_output_specmap_id, 
-                                                            path_to_output_cnt_to_spec=path_to_output_cnt_to_spec,
-                                                           )
+    return all_paths, rewrite    
     
     
+def process_files(args) :
     
+    , rewrite_dict = read_paths(args)
     
+    # parse_uniprot()
+    fmanip = Fasta_manipulations(args)
     
-    return 0
+    if path.exists(fmanip.path_to_uniprot_taxid_set) and (not rewrite_dict['taxid_set']) :
+        fmanip.uniprot_taxid_set = load(fmanip.path_to_uniprot_taxid_set)
+    else :
+        fmanip.prepare_uniprot_taxid_set(dump=True)
+    if path.exists(fmanip.path_to_sprot_taxid_set) and (not rewrite_dict['taxid_set']) :
+        fmanip.sprot_taxid_set = load(fmanip.path_to_sprot_taxid_set)
+    else :
+        fmanip.prepare_sprot_taxid_set(dump=True)
+        
+    if path.exists(fmanip.path_to_len_fasta_uniprot) and (not rewrite_dict['len_fasta']) :
+        fmanip.len_fasta_uniprot = load(fmanip.path_to_len_fasta_uniprot)
+    else :
+        fmanip.calc_lens_for_uniprot_taxid_set(dump=True)
+    if path.exists(fmanip.path_to_len_fasta_sprot) and (not rewrite_dict['len_fasta']) :
+        fmanip.len_fasta_sprot = load(fmanip.path_to_len_fasta_sprot)
+    else :
+        fmanip.calc_lens_for_sprot_taxid_set(dump=True)
+    
+    if path.exists(fmanip.path_to_species_descendants) and (not rewrite_dict['species_descendants']) :
+        fmanip.species_descendants = load(fmanip.path_to_species_descendants)
+    else :
+        fmanip.get_descendents_dict(allowed_ranks, dump=True)
+
+    if path.exists(fmanip.path_to_leaders_uniprot) and (not rewrite_dict['leaders_uniprot']) :
+        fmanip.species_leader_uniprot = load(fmanip.path_to_leaders_uniprot)
+    else :
+        fmanip.get_leaders_uniprot(dump=True)
+    if path.exists(fmanip.path_to_leaders_sprot) and (not rewrite_dict['leaders_sprot']) :
+        fmanip.species_leader_sprot = load(fmanip.path_to_leaders_sprot)
+    else :
+        fmanip.get_leaders_sprot(dump=True)
+    fmanip.leaders = set(fmanip.species_leader_sprot.values()).union(set(fmanip.species_leader_uniprot.values()))
+    
+    if path.exists(fmanip.path_to_exclude_names) and (not rewrite_dict['exclude_names']) :
+        fmanip.exclude_names = load(fmanip.path_to_exclude_names)
+    else :
+        fmanip.exclude_wrong(dump=True)
+    taxid_count = fmanip.write_10_perc_fasta(treshold=200)
+    
+    mzml_generation()
+    feature_generation()
+    
+    blind_search()
+    
+    ms1searchpy()
+    directMS1()
+    generate_images()
